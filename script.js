83d9f414e3705aa0a1e195f214a78f6ea7dbccea
jp-martinezc <jp.martinezc@users.noreply.github.com>
2021-02-10 19:49:00 -0500

    ayuda

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
  
-/* Toggle mobile menu */
+/* menu mobil */
 function toggleMenu() {
     if (menu.classList.contains("active")) {
         menu.classList.remove("active");
          
-        // adds the menu (hamburger) icon
+        
         toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
     } else {
         menu.classList.add("active");
          
-        // adds the close (x) icon
+        // anade el selector de cierre
         toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
     }
 }
